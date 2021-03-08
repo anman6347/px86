5908a781e15a7017121a87d4833867e733ef26e4
anman6347 <4620096@ed.tus.ac.jp>
2021-03-09 02:32:09 +0900

    feat: 1 文字入出力の仮想的な実装

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 TARGET = px86
-BIN = test.bin
-SRCS = main.c instruction.c modrm.c emulator_function.c emulator.h emulator_function.h instruction.h modrm.h
-OBJS = main.o instruction.o modrm.o emulator_function.o
+BIN = helloworld.bin
+SRCS = main.c instruction.c modrm.c emulator_function.c io.c emulator.h emulator_function.h instruction.h modrm.h io.h
+OBJS = main.o instruction.o modrm.o emulator_function.o io.o
 
 NASM = ../tolset_p86/tolset_p86/z_tools/nasm.exe
 LD = ld
 CC = clang-10
 CFLAGS = -Wall
+
+all: $(TARGET) $(BIN)
 
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $@ $^
@@ -14,10 +16,13 @@ $(TARGET): $(OBJS)
 %.o : %.c
 	$(CC) $(CFLAGS) -c $<
 
+$(BIN): helloworld.asm
+	$(NASM) -f bin -o $@ $<
+
 clean :
-	rm $(TARGET) $(OBJS)
+	rm $(TARGET) $(OBJS) $(BIN)
 
 test: 
-	./$(TARGET) $(BIN)
+	./$(TARGET) -q $(BIN)
 
-.PHONY : clean test
+.PHONY : all clean test
--- instruction.c
+++ instruction.c
@@ -7,6 +7,7 @@
 
 #include "emulator.h"
 #include "emulator_function.h"
+#include "io.h"
 #include "modrm.h"
 
 /* x86命令の配列、opecode番目の関数がx86の
@@ -57,6 +58,22 @@ static void cmp_rm32_imm8(Emulator* emu, ModRM* modrm) {
     update_eflags_sub(emu, rm32, imm8, result);
 }
 
+static void cmp_al_imm8(Emulator* emu) {
+    uint8_t value = get_code8(emu, 1);
+    uint8_t al = get_register8(emu, AL);
+    uint64_t result = (uint64_t)al - (uint64_t)value;
+    update_eflags_sub(emu, al, value, result);
+    emu->eip += 2;
+}
+
+static void cmp_eax_imm32(Emulator* emu) {
+    uint32_t value = get_code32(emu, 1);
+    uint32_t eax = get_register32(emu, EAX);
+    uint64_t result = (uint64_t)eax - (uint64_t)value;
+    update_eflags_sub(emu, eax, value, result);
+    emu->eip += 5;
+}
+
 /*  REG の値がオペコードの拡張となっている sub_rm32_imm8 の処理用関数*/
 static void code_83(Emulator* emu) {
     emu->eip += 1;
@@ -81,6 +98,12 @@ static void code_83(Emulator* emu) {
 static void inc_rm32(Emulator* emu, ModRM* modrm) {
     uint32_t value = get_rm32(emu, modrm);
     set_rm32(emu, modrm, value + 1);
+}
+
+static void inc_r32(Emulator* emu) {
+    uint8_t reg = get_code8(emu, 0) - 0x40;
+    set_register32(emu, reg, get_register32(emu, reg) + 1);
+    emu->eip += 1;
 }
 
 /*  REG の値がオペコードの拡張となっている inc_rm32 の処理用関数*/
@@ -131,6 +154,28 @@ static void mov_rm32_imm32(Emulator* emu) {
     uint32_t value = get_code32(emu, 0);
     emu->eip += 4;
     set_rm32(emu, &modrm, value);
+}
+
+static void mov_r8_imm8(Emulator* emu) {
+    uint8_t reg = get_code8(emu, 0) - 0xB0;
+    set_register8(emu, reg, get_code8(emu, 1));
+    emu->eip += 2;
+}
+
+static void mov_rm8_r8(Emulator* emu) {
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t r8 = get_r8(emu, &modrm);
+    set_rm8(emu, &modrm, r8);
+}
+
+static void mov_r8_rm8(Emulator* emu) {
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t rm8 = get_rm8(emu, &modrm);
+    set_r8(emu, &modrm, rm8);
 }
 
 /* ジャンプ系 */
@@ -214,6 +259,21 @@ static void leave(Emulator* emu) {
     emu->eip += 1;
 }
 
+/* I/O 系 */
+static void in_al_dx(Emulator* emu) {
+    uint16_t address = get_register32(emu, EDX) & 0xffff;  // EDX の下位 16bit DX
+    uint8_t value = io_in8(address);
+    set_register8(emu, AL, value);
+    emu->eip += 1;
+}
+
+static void out_dx_al(Emulator* emu) {
+    uint16_t address = get_register32(emu, EDX) & 0xffff;
+    uint8_t value = get_register8(emu, AL);
+    io_out8(address, value);
+    emu->eip += 1;
+}
+
 /* 関数ポインタテーブルの初期化 */
 void init_instructions() {
     int i;
@@ -223,6 +283,11 @@ void init_instructions() {
     /* 各命令の割り当て */
     instructions[0x01] = add_rm32_r32;
     instructions[0x3B] = cmp_r32_rm32;
+    instructions[0x3C] = cmp_al_imm8;
+    instructions[0x3D] = cmp_eax_imm32;
+    for (i = 0; i < 8; i++) {
+        instructions[0x40 + i] = inc_r32;
+    }
     for (i = 0; i < 8; i++) {
         instructions[0x50 + i] = push_r32;
     }
@@ -242,8 +307,13 @@ void init_instructions() {
     instructions[0x7C] = jl;
     instructions[0x7E] = jle;
     instructions[0x83] = code_83;
+    instructions[0x88] = mov_rm8_r8;
     instructions[0x89] = mov_rm32_r32;
+    instructions[0x8A] = mov_r8_rm8;
     instructions[0x8B] = mov_r32_rm32;
+    for (i = 0; i < 8; i++) {
+        instructions[0xB0 + i] = mov_r8_imm8;
+    }
     for (i = 0; i < 8; i++) {
         instructions[0xB8 + i] = mov_r32_imm32;
     }
@@ -253,5 +323,7 @@ void init_instructions() {
     instructions[0xE8] = call_rel32;
     instructions[0xE9] = near_jump;
     instructions[0xEB] = short_jump;
+    instructions[0xEC] = in_al_dx;
+    instructions[0xEE] = out_dx_al;
     instructions[0xFF] = code_ff;
 }
